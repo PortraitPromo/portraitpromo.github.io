1497325840d4a64c48e3644e416939fc4ea2c3b1
Dmitry Fyodorov <fyodrik@yandex.ru>
2020-06-24 04:59:25 +0300

    test105
    test105

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -173,9 +173,9 @@ $(function() {
 });
 
 //12. TEMPLATER
-$('#launcher').load('launcher/app.html');
-$('header').load('header.html');
-$('footer').load('footer.html');
+$('#launcher').load('/launcher/app.html');
+$('header').load('/header.html');
+$('footer').load('/footer.html');
 
 // wave
 $(function(){
